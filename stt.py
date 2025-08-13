4df9e9459a08339be8c7519e3e41fd7bd36ddda2
arish420 <muhammadarish420@gmail.com>
2025-08-13 23:17:07 +0500

    Update stt.py

--- stt.py
+++ stt.py
@@ -11,4 +11,4 @@ st.title("Speech to Text AI")
 uploaded_audio = st.file_uploader("Upload an audio file", type=["mp3", "wav", "ogg"])
 
 if uploaded_audio:
-  st.write(uploaded_audio)
+  st.write(uploaded_audio.read())
