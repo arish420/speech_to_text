1adf0d8b3452f29247b6163e6f579a04d1500fdc
arish420 <muhammadarish420@gmail.com>
2025-08-13 23:14:27 +0500

    Update stt.py

--- stt.py
+++ stt.py
@@ -6,3 +6,9 @@ import streamlit as st
 
 
 st.title("Speech to Text AI")
+
+
+uploaded_audio = st.file_uploader("Upload an audio file", type=["mp3", "wav", "ogg"])
+
+if uploaded_audio:
+  st.write(uploaded_audio)
