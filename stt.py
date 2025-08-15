30b119e707a92a8d2624aae7dde688c3d57a77c3
arish420 <muhammadarish420@gmail.com>
2025-08-15 18:05:54 +0500

    Update stt.py

--- stt.py
+++ stt.py
@@ -8,20 +8,32 @@ import re
 # Load environment variables
 load_dotenv()
 
+
+#groq
+# https://docs.google.com/spreadsheets/d/1R01qyrYpZn9CUtSGiI0IWysXwY7yP-jKo7YZ4xhu1uE/edit?gid=0#gid=0
+
 sheet_id = '1R01qyrYpZn9CUtSGiI0IWysXwY7yP-jKo7YZ4xhu1uE' # replace with your sheet's ID
 url=f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv"
 df_groq=pd.read_csv(url)
 
 
-# https://docs.google.com/spreadsheets/d/1R01qyrYpZn9CUtSGiI0IWysXwY7yP-jKo7YZ4xhu1uE/edit?gid=0#gid=0
+sheet_id = '150_Ht0_ecl8EwQa14sne4U_C8N0nPYoNhIpIW0dG7Mc' # replace with your sheet's ID
+url=f"https://docs.google.com/spreadsheets/d/{sheet_id}/export?format=csv"
+df_openai=pd.read_csv(url)
 
-os.environ["OPENAI_API_KEY"] =  df_groq.keys()[0]
+#openai
+#https://docs.google.com/spreadsheets/d/150_Ht0_ecl8EwQa14sne4U_C8N0nPYoNhIpIW0dG7Mc/edit?usp=sharing
+
+os.environ["OPENAI_API_KEY"] =  df_openai.keys()[0]
+os.environ["GROQ_API_KEY"] =  df_groq.keys()[0]
+
+
 
 
 
 st.title("Speech to Text AI")
-client = OpenAI()
-# client = Groq()
+client1 = OpenAI()
+client = Groq()
 uploaded_file = st.file_uploader("Upload an audio file", type=["mp3", "wav", "ogg"])
 
 col1, col2 = st.columns(2)
@@ -29,6 +41,9 @@ with col1:
     selection = st.selectbox("Select Output Language", ("Dutch","Spanish","French"))
 with col2:
     translation_option = st.selectbox("Select Translation Option", ("Whisper","GPT-4o-transcribe"))
+
+
+platform = st.selectbox("Select Platform", ("Groq","OpenAI"))
 
 
 lang_dict = {"Dutch":"nl","Spanish":"es","French":"fr"}
@@ -42,12 +57,21 @@ if st.button("Sumbit"):
             f.write(uploaded_file.getbuffer())
         input_filepath = "temp_audio_input." + uploaded_file.name.split(".")[-1]
 
-
         
-        audio_file = open(input_filepath, "rb")
-        transcription = client.audio.transcriptions.create(
-          file=audio_file,
-          model=translation_dict[translation_option])
+        if platform == "OpenAI":
+            audio_file = open(input_filepath, "rb")
+            transcription = client1.audio.transcriptions.create(file=audio_file,model=translation_dict[translation_option])
+            st.write(transcription.text)
+
+        else:
+            with open(input_filepath, "rb") as file:
+              transcription = client.audio.transcriptions.create(file=(input_filepath, file.read()),
+              model="whisper-large-v3",
+              language=lang_dict[selection],
+              response_format="verbose_json",)
+              st.write(transcription.text)
+
+            
 
 
         # segments = transcription.segments
@@ -73,8 +97,23 @@ if st.button("Sumbit"):
         # for s in sentences:
         #     st.markdown(f"**[{s['start']:.2f} - {s['end']:.2f}]** {s['text']}")
 
-        st.write(transcription.text)
-      
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
         # with open(input_filepath, "rb") as file:
         #       transcription = client.audio.transcriptions.create(
         #       file=(input_filepath, file.read()),
